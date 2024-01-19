88eea454959680130e4da664354a6ae08f0b86de
Edward E Greenwood <edwardgreenwood38@gmail.com>
2024-01-18 19:20:03 -0500

    step 7 completed

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ const app = express();
 app.get('/', function (req, res) {
     res.send('Hello world');
 });
+
+
+// listen on port 3000 for server
+app.listen(3000, () => {
+    console.log('Web server started');
+});
